cc8dc62a848f9f8782f1d07b6798e799249eabde
alextheceo <alextheceo@outlook.com>
2025-04-23 14:45:36 -0400

    Fixed yet another build issue

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
